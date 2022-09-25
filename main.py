5ac9a59cd19fc8a207d00df936fa3e6262ad531d
Viktor <viktor_polyak@mail.ru>
2022-09-25 16:04:17 +0300

    Проект создан

---
+++ main.py
@@ -0,0 +1,56 @@
+from re import search
+from req import *
+
+
+def parse_str(text: str) -> tuple:
+    """
+    Проверяет корректность запроса и разбивает его на отдельные элементы
+    :param text: запрос
+    :return: кортеж с запросами и их параметрами
+    """
+    correct_queries = ["filter", "map", "unique", "sort", "limit"]
+    queries = search(r"^(\S+) (\S+) \| (\S+) (\S+)$", text)
+    if queries:
+        if queries[1] in correct_queries and queries[3] in correct_queries:
+            return queries[1], queries[2], queries[3], queries[4]
+    return None, None, None, None
+
+
+def read_file() -> list:
+    """
+    :return: список строк из файла
+    """
+    with open("./data/apache_logs.txt") as file:
+        content = file.read()
+    return content.strip().split("\n")
+
+
+def execute_query(logs_lines: Iterator, query: str, param: str) -> Iterator:
+    if query == "filter":
+        return filter(lambda s: param in s, logs_lines)
+    elif query == "map":
+        return Query.q_map(logs_lines, param)
+    elif query == "unique" and param == "-":
+        return Query.q_unique(logs_lines)
+    elif query == "sort":
+        return  Query.q_sort(logs_lines, param)
+    elif query == "limit":
+        return Query.q_limit(logs_lines, param)
+    else:
+        return logs_lines
+
+
+def main() -> None:
+    logs_lines = iter(read_file())
+    query = input(">>> ")
+    query1, param1, query2, param2 = parse_str(query)
+    if not query1:
+        return None
+    logs_lines = execute_query(logs_lines, query1, param1)
+    logs_lines = execute_query(logs_lines, query2, param2)
+    [print(line) for line in list(logs_lines)]
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ req.py
@@ -0,0 +1,58 @@
+from typing import Iterator
+
+class Query:
+    @staticmethod
+    def q_map(logs: Iterator, param: str) -> Iterator:
+        """
+        Итератор возвращающий нужный столбец
+        :param logs: входной итератор
+        :param param: номер столбца
+        :return: итератор по столбцу
+        """
+        for line in logs:
+            try:
+                yield line.split()[int(param)]
+            except:
+                continue
+
+
+    @staticmethod
+    def q_unique(logs: Iterator) -> Iterator:
+        """
+        итератор оставляющий только уникальные строки
+        :param logs: входной итератор
+        :return: итератор уникальных строк
+        """
+        logs = iter(set(logs))
+        for line in logs:
+            yield line
+
+    @staticmethod
+    def q_sort(logs: Iterator, param: str) -> Iterator:
+        """
+        итератор сортировки
+        :param logs: входной итератор
+        :param param: строка 'asc' для сортировки по возрастанию или 'desc' для обратной сортировки
+        :return:
+        """
+        if param == "asc":
+            return iter(sorted(logs))
+        elif param == "desc":
+            return iter(sorted(logs, reverse=True))
+        else:
+            return logs
+
+    @staticmethod
+    def q_limit(logs: Iterator, param: str) -> Iterator:
+        """
+        итератор ограничивающий кол-во выводимых строк
+        :param logs: входной итератор
+        :param param: кол-во строк
+        :return: итератор из нужного кол-ва строк
+        """
+        for _ in range(int(param)):
+            try:
+                yield logs.__next__()
+            except StopIteration:
+                break
+
